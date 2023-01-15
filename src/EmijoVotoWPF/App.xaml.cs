05604d57c2b8ae5d0a105b1d163c50ffad773a9d
Brave Cobra <bravecobra@gmail.com>
2023-01-15 06:22:26 +0100

    Added WPF

---
+++ src/EmijoVotoWPF/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using EmojiVotoWPF.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmojiVotoWPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainWindow = ConfigurationRoot.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+        base.OnStartup(e);
+    }
+}
---
+++ src/EmijoVotoWPF/Configuration/ConfigurationRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmojiVotoWPF.Configuration
+{
+    internal static class ConfigurationRoot
+    {
+        private static IServiceProvider? _serviceProvider;
+
+        public static IServiceProvider Services => _serviceProvider ??= Run();
+
+        private static IServiceProvider Run()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
